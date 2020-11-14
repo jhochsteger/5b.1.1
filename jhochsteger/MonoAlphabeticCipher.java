c3783cb1d06098531278f2b6586a445a4461b93a
jhochsteger <jonas.hochsteger.04@gmail.com>
2020-11-14 18:31:15 +0100

    Finished the guts from MonoAlphabeticCipher

--- jhochsteger/MonoAlphabeticCipher.java
+++ jhochsteger/MonoAlphabeticCipher.java
@@ -6,27 +6,36 @@ package jhochsteger;
  * @version 14-11-2020
  */
 public class MonoAlphabeticCipher implements Cipher{
-    private String secretAlphabet;
+    private String secretAlphabet, standardsecretAlphabet;
 
     public MonoAlphabeticCipher(){
-
+        secretAlphabet = "abcdefghijklmnopqrstuvwxyzäöüß";
+        standardsecretAlphabet = "abcdefghijklmnopqrstuvwxyzäöüß";
     }
 
     public String getSecretAlphabet() {
-        return null;
+        return secretAlphabet;
     }
 
     protected void setSecretAlphabet(String secretAlphabet) {
-
+        this.secretAlphabet = secretAlphabet;
     }
 
     @Override
     public String encrypt(String text) {
-        return null;
+        String encrypted = "";
+        for (int i = 0; i < text.length(); i++) {
+            encrypted += secretAlphabet.charAt(standardsecretAlphabet.indexOf(text.charAt(i)));
+        }
+        return encrypted;
     }
 
     @Override
     public String decrypt(String text) {
-        return null;
+        String decrypted = "";
+        for (int i = 0; i < text.length(); i++) {
+            decrypted += standardsecretAlphabet.charAt(secretAlphabet.indexOf(text.charAt(i)));
+        }
+        return decrypted;
     }
 }
--- jhochsteger/SubstitutionCipher.java
+++ jhochsteger/SubstitutionCipher.java
@@ -1,5 +1,10 @@
 package jhochsteger;
 
+/**
+ * A Class that extends the class MonoAlphabeticCipher.
+ * @author Jonas Hochsteger
+ * @version 14-11-2020
+ */
 public class SubstitutionCipher extends MonoAlphabeticCipher{
     public SubstitutionCipher(String secretAlphabet){
 
