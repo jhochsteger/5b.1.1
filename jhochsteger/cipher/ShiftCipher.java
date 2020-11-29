40f2f6f6a4045ebc4ee04eab3943b1a91d7d5640
jhochsteger <jonas.hochsteger.04@gmail.com>
2020-11-29 10:39:12 +0100

    Added comments to the Class ShiftCipher

--- jhochsteger/cipher/ShiftCipher.java
+++ jhochsteger/cipher/ShiftCipher.java
@@ -1,10 +1,24 @@
 package jhochsteger.cipher;
 
+/**
+ * A Class that extends the class MonoAlphabeticCipher.
+ *
+ * @author Jonas Hochsteger
+ * @version 29-11-2020
+ */
 public class ShiftCipher extends MonoAlphabeticCipher{
+    /**
+     * A Constructor for the class ShiftCipher
+     * @param value the Value for how far the alphabet gets shiftet
+     */
     public ShiftCipher(int value) {
         setShiftValue(value);
     }
 
+    /**
+     * A setter Method that generates a secrec Alphabet thats shiftet by value
+     * @param value
+     */
     public void setShiftValue(int value){
         String standardsecretAlphabet = "abcdefghijklmnopqrstuvwxyzäöüß";
         String secretAlphabet = "";
