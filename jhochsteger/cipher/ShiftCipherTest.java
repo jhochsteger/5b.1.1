dfd03de5d945f837a31efafe012ccedd6a6315f4
jhochsteger <jonas.hochsteger.04@gmail.com>
2020-11-29 10:44:16 +0100

    Added tests for ShiftCipher

---
+++ jhochsteger/cipher/ShiftCipherTest.java
@@ -0,0 +1,21 @@
+package jhochsteger.cipher;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ShiftCipherTest {
+    @Test
+    public void ShiftCipherTest_setSecretAlphabet_true() {
+        String cipher = "fghijklmnopqrstuvwxyzäöüßabcde";
+        ShiftCipher shiftCipher = new ShiftCipher(5);
+        assertEquals(cipher, shiftCipher.getSecretAlphabet());
+    }
+
+    @Test
+    public void ShiftCipherTest_setSecretAlphabet_false() {
+        String cipher = "abcdefghijklmnopqrstuvwxyzäöüß";
+        ShiftCipher shiftCipher = new ShiftCipher(5);
+        assertNotEquals(cipher, shiftCipher.getSecretAlphabet());
+    }
+}
